1894020e8df2960ebc43c23d86fad9b5f48eabdf
sanjay <sanjaypython22@gmail.com>
2023-06-23 12:23:40 +0530

    gallery model

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from homepage.models import LookupField
+from homepage.models import LookupField,Gallery
 # Register your models here.
 admin.site.register(LookupField)
+admin.site.register(Gallery)
 
--- homepage/models.py
+++ homepage/models.py
@@ -7,6 +7,17 @@ class LookupField(models.Model):
     desc = models.TextField()
     date = models.DateField(auto_now_add=True)
     img = models.ImageField(upload_to='lookup_img/', null=True)
+    pdf = models.FileField(upload_to='pdf', default=None,null=True)
 
     def __str__(self):
         return self.code
+    
+class Gallery(models.Model):
+    code  = models.CharField(max_length=100)
+    title  = models.CharField(max_length=1000)
+    desc = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    img = models.ImageField(upload_to='lookup_img/', null=True)
+
+    def __str__(self):
+        return self.title

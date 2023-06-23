2fe3d9ac1d110408e7266f2e9e88fd1e2727c7b8
sanjay <sanjaypython22@gmail.com>
2023-06-23 12:37:36 +0530

    views.py fiel

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from accounts.models import EnquiryDetails
-from homepage.models import LookupField
+from homepage.models import LookupField,Gallery
 from services.models import Service
 
 # Create your views here.
@@ -76,7 +76,7 @@ def add_gallery(request):
         image_files = request.FILES.getlist('image_files')
         try:
             for i in range(len(image_files)):
-                LookupField.objects.create(code=code,title=title,desc=description,img=image_files[i])
+                Gallery.objects.create(code=code,title=title,desc=description,img=image_files[i])
             return redirect('/')
         except Exception as e:
             print(e,'-------------e')

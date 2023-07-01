71dbde57f5cfb5c9a122abed0fcd3f5a7fe8e4cc
sanjay <sanjaypython22@gmail.com>
2023-07-01 18:37:17 +0530

    files

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from homepage.models import LookupField,Gallery
+from homepage.models import LookupField,Gallery,Org
 
 # Register your models here.
 
+admin.site.register(Org)
 admin.site.register(LookupField)
 admin.site.register(Gallery)
 

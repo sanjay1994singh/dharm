ac15d28f7683aad2ac69d1f78bcaaeb94bb9c065
sanjay <sanjaypython22@gmail.com>
2023-07-10 20:35:11 +0530

    new require

--- account/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('join-member/',views.join_member, name='join_member'),
-    path('success/',views.success, name='success'),
-]

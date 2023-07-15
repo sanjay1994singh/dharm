9ebb4d8bc8bb98615e5ee1393d4e7dcc3ffe9d57
sanjay <sanjaypython22@gmail.com>
2023-07-15 15:11:51 +0530

    sangthan post

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('join-member/',views.join_member, name='join_member'),
+    path('add-member/<int:id>/',views.add_member, name='add_member'),
+    path('free-member/',views.free_member, name='free_member'),
     path('success/',views.success, name='success'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,8 +2,18 @@ import razorpay
 from django.shortcuts import render
 from .models import CustomUser,MemberType,Gender
 from django.conf import settings
+from django.shortcuts import redirect
 # Create your views here.
+
 def join_member(request):
+    member_type = MemberType.objects.all()
+    context = {
+        'member_type':member_type
+    }
+    return render(request,'join_member.html',context)
+
+
+def add_member(request,id):
     if request.method == 'POST':
         form = request.POST
         name = form.get('name')
@@ -33,9 +43,15 @@ def join_member(request):
                                         id_number=id_number,
                                         )
     else:
+        member = MemberType.objects.get(id=id)
+        mem_type = member.type
+        if mem_type == 'हितचिंतक':
+            return redirect('/account/free-member/')
+        
         member_type = MemberType.objects.all()
+       
         gender_type = Gender.objects.all()
-        amount = 100
+        amount = member.price
 
         client = razorpay.Client(auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET))
         payment = client.order.create({'amount': int(amount) * 100, 'currency': 'INR', 'payment_capture': '1'})
@@ -44,9 +60,46 @@ def join_member(request):
         context = {
             'payment':payment,
             'member_type':member_type,
+            'member_price':amount,
             'gender_type':gender_type,
         }
-        return render(request,'join_member.html',context)
+        return render(request,'add_member.html',context)
+    
+def free_member(request):
+    if request.method == 'POST':
+        form = request.POST
+        name = form.get('name')
+        email = form.get('email')
+        phone = form.get('phone')
+        address = form.get('address')
+        pincode = form.get('pincode')
+        city = form.get('city')
+        district = form.get('district')
+        state = form.get('state')
+        country = form.get('country')
+        dob = form.get('dob')
+        gender = form.get('gender')
+        id_number = form.get('id_number')
+        obj = CustomUser.objects.create(name=name,
+                                        email=email,
+                                        phone=phone,
+                                        address=address,
+                                        pincode=pincode,
+                                        city=city,
+                                        district=district,
+                                        state=state,
+                                        country=country,
+                                        dob=dob,
+                                        gender_id=gender,
+                                        # member_type_id=member_type,
+                                        id_number=id_number,
+                                        )
+    else:
+        member_type = MemberType.objects.all()
+        context = {
+            'member_type':member_type
+        }
+        return render(request,'free_member.html',context)
 
 def success(request):
     if request.method == 'GET':
@@ -54,11 +107,7 @@ def success(request):
         razorpay_order_id = form.get('razorpay_order_id', None)
         razorpay_payment_id = form.get('razorpay_payment_id', None)
         razorpay_signature = form.get('razorpay_signature', None)
-        course_id = form.get('course_id', None)
         price = form.get('price', None)
-        discount = form.get('discount', None)
-        totalprice = form.get('totalprice', None)
-        quantity = form.get('quantity', None)
         payment_status = form.get('payment_status', None)
         try:
             pass

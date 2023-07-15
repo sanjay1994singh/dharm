a02f017dbf31793605ce77d950d3708b64ee9e1a
sanjay <sanjaypython22@gmail.com>
2023-07-15 16:23:20 +0530

    sangthan post

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,4 @@ urlpatterns = [
     path('join-member/',views.join_member, name='join_member'),
     path('add-member/<int:id>/',views.add_member, name='add_member'),
     path('free-member/',views.free_member, name='free_member'),
-    path('success/',views.success, name='success'),
 ]

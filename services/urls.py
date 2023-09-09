b98e1b96835eb12208a84acbf00945ff8a75dd84
sanjay <sanjaypython22@gmail.com>
2023-09-09 11:46:07 +0530

    'kk'

--- services/urls.py
+++ services/urls.py
@@ -3,17 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('contact/', views.contact, name='contact'),
-    path('service_details/<int:id>/', views.service_details, name='service_details'),
-    path('biography/<int:id>/', views.biography, name='biography'),
-    path('add-gallery/', views.add_gallery, name='add_gallery'),
-    path('gallery/', views.gallery, name='gallery'),
-    path('photo-gallery/', views.photo_gallery, name='photo_gallery'),
-    path('samachar-gallery/', views.samachar_gallery, name='photo_gallery'),
-    path('viewPdf/', views.viewPdf, name='viewPdf'),
-    path('sangthan/', views.sangthan_suchi, name='sangthan_suchi'),
-    path('get_sangth_padname/', views.get_sangth_padname, name='get_sangth_padname'),
-    path('get_sangth/', views.get_sangth, name='get_sangth'),
-    path('sangthan_list/<int:id>/', views.sangthan_list, name='sangthan_list'),
+    path('puja_form/', views.puja_form, name='puja_form'),
+    path('Jyotish_form/', views.Jyotish_form, name='Jyotish_form'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def puja_form(request):
+    return render(request, 'puja_form.html')
+
+def Jyotish_form(request):
+    return render(request, 'jyotish_form.html')

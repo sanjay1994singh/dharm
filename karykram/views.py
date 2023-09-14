32c773b9c3a62b04f89a81fc6ffd285e84415737
sanjay <sanjaypython22@gmail.com>
2023-09-14 20:05:47 +0530

    'k'

--- karykram/views.py
+++ karykram/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 from .models import Karykram
-
+from homepage.models import LookupField
 
 # Create your views here.
 def karykram(request, type):
+    title_logo_data = LookupField.objects.get(code='TITLE')
     karykram = Karykram.objects.filter(type__type=type)
     context = {
-        'karykram': karykram
+        'title_data': title_logo_data,
+        'karykram': karykram,
     }
     return render(request, 'karykram.html', context)

4c9aca9ece47b0c7d8ccba3d21752f099c186c2c
sanjay <sanjaypython22@gmail.com>
2023-10-21 16:17:41 +0530

    i

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import LookupField, Gallery, Org, SangthanType, Sangthan, Post
+from .models import LookupField, Gallery, Org, SangthanType, Sangthan, Post, ImageFolder, ImageGallery
 
 # Register your models here.
 
+admin.site.register(ImageFolder)
+admin.site.register(ImageGallery)
 admin.site.register(Org)
 admin.site.register(LookupField)
 admin.site.register(Gallery)

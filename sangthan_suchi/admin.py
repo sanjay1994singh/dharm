23b8c7e5f078e23d533518f0c2718de37a816bb4
nidhivan ras <nidhivanras@gmail.com>
2023-12-23 13:25:09 +0530

    p

--- sangthan_suchi/admin.py
+++ sangthan_suchi/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Pradesh, PradeshPadadhikari, DistrictPadadhikari, District, NagarPadadhikari, Nagar
 
 # Register your models here.
+admin.site.register(Pradesh)
+admin.site.register(PradeshPadadhikari)
+admin.site.register(District)
+admin.site.register(DistrictPadadhikari)
+admin.site.register(Nagar)
+admin.site.register(NagarPadadhikari)
